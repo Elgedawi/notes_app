0c17ad7a4031788faa851a65590a53120929a8c1
elgedawi <amostafag45@gmail.com>
2025-11-24 10:50:53 +0200

    make note sheet scroolable

--- lib/widgets/add_note_sheet.dart
+++ lib/widgets/add_note_sheet.dart
@@ -9,23 +9,25 @@ class AddNoteSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(vertical: 25, horizontal: 20),
-      child: Column(
-        children: [
-          SizedBox(height: 10),
-          CustomTextfield(
-            height: 50,
-            hint: 'Title',
-            alignment: Alignment.centerLeft,
-          ),
-          SizedBox(height: 14),
-          CustomTextfield(
-            height: 150,
-            hint: 'content',
-            alignment: Alignment.centerLeft,
-          ),
-          SizedBox(height: 150),
-          CustomTextButton(),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            CustomTextfield(
+              height: 60,
+              hint: 'Title',
+              alignment: Alignment.centerLeft,
+            ),
+            SizedBox(height: 14),
+            CustomTextfield(
+              height: 150,
+              hint: 'content',
+              alignment: Alignment.centerLeft,
+            ),
+            SizedBox(height: 120),
+            CustomTextButton(),
+          ],
+        ),
       ),
     );
   }
