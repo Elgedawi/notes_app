cc3a9dc37e1f15a8a741d74e7ea79bebafe0215a
elgedawi <amostafag45@gmail.com>
2025-11-25 21:12:36 +0200

    integrrate cubit with add note sheet

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/constants.dart';
@@ -8,6 +8,7 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+  bool isloading = false;
   addNote(NoteModel note) async {
     emit(AddNoteILoading());
     try {
--- lib/widgets/add_note_sheet.dart
+++ lib/widgets/add_note_sheet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/custom_form.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/widgets/custom_add_note_form.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddNoteSheet extends StatelessWidget {
   const AddNoteSheet({super.key});
@@ -8,8 +11,26 @@ class AddNoteSheet extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 25, horizontal: 20),
       child: SingleChildScrollView(
-        child: CustomForm(),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteFailure) {
+              showSnackBar(context, state);
+            } else if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+                inAsyncCall: state is AddNoteILoading ? true : false,
+                child: const CustomForm());
+          },
+        ),
       ),
     );
+  }
+
+  void showSnackBar(BuildContext context, AddNoteFailure state) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(state.errMessage)));
   }
 }
